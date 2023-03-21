466848073675326ae69418ffa0d8749dd978d469
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-03-21 13:20:42 +0800

    Save theme to local storage.

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,19 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DarkTheme from './DarkTheme.styled';
+
+function loadTheme() {
+  if (typeof localStorage === 'undefined') {
+    return false;
+  }
+
+  const value = localStorage.getItem('darkMode') ?? 'false';
+  return JSON.parse(value);
+}
 
 export default function ThemeSwitch() {
   const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    const loadedTheme = loadTheme();
+    setDarkMode(loadedTheme);
+  }, []);
   const text = darkMode ? 'Light Mode' : 'Dark Mode';
-  console.log('[ThemeSwitch] darkmode: ', darkMode);
+  console.log('[ThemeSwitch] dark mode: ', darkMode);
+
+  const handleSwitch = () => {
+    setDarkMode((prev) => {
+      localStorage.setItem('darkMode', JSON.stringify(!prev));
+      return !prev;
+    });
+  };
 
   return (
     <>
-      <button
-        className='themeSwitchButton'
-        onClick={() => setDarkMode((prev) => !prev)}
-      >
-        {text}
-      </button>
+      <button onClick={handleSwitch}>{text}</button>
       <style jsx>{`
         button {
           background: none;
